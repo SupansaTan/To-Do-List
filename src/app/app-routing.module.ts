3e3bf49a52bcd07b0ba39efe8f9c1737145afa1d
SupansaTan <s6201012620236@email.kmutnb.ac.th>
2021-04-08 00:32:21 +0700

    Can tap add icon to add task page and change background color

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router'
 import { NativeScriptRouterModule } from '@nativescript/angular'
 
 import { HomeComponent } from './pages/home/home.component'
+import { AddTaskComponent } from './pages/add-task/add-task.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'add', component: AddTaskComponent },
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,14 @@ import { AppComponent } from './app.component'
 import { HomeComponent } from './pages/home/home.component'
 import { TopBarComponent } from './components/top-bar/top-bar.component'
 import { TaskListComponent } from './components/task-list/task-list.component'
+import { AddTaskComponent } from './pages/add-task/add-task.component'
 
 import { TaskService } from './task.service'
 
 @NgModule({
   bootstrap: [AppComponent],
   imports: [NativeScriptModule, AppRoutingModule ],
-  declarations: [AppComponent, HomeComponent, TopBarComponent, TaskListComponent ],
+  declarations: [AppComponent, HomeComponent, TopBarComponent, TaskListComponent, AddTaskComponent],
   providers: [TaskService],
   schemas: [NO_ERRORS_SCHEMA],
 })
