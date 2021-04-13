6268ade466cbf13b229d72d8f51e3e05a5bddcdf
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2021-04-14 02:23:05 +0700

    Picker images from device to add detail to task

--- src/app/pages/add-task/add-task.component.ts
+++ src/app/pages/add-task/add-task.component.ts
@@ -3,11 +3,11 @@ import { TaskService } from "~/app/task.service";
 import { DatePipe } from '@angular/common'
 import { Location } from '@angular/common'
 import * as camera from "@nativescript/camera";
-import { Image, ImageAsset } from "@nativescript/core";
-import { ImageSource, knownFolders, path } from '@nativescript/core';
+import { ImageAsset } from "@nativescript/core";
+import { ImageSource, knownFolders} from '@nativescript/core';
 import { flatMap, map } from 'rxjs/operators';
 import { from } from 'rxjs';
-import * as fs from "@nativescript/core/file-system";
+import * as imagepicker from "@nativescript/imagepicker";
 
 @Component ({
     selector: "add-task",
@@ -35,6 +35,7 @@ export class AddTaskComponent {
         this.time = new Date();
         this.showButtons = false;
         this.hasImage = false;
+        this.task_photo = []
     }
 
     public add() {
@@ -57,23 +58,73 @@ export class AddTaskComponent {
             flatMap(() => camera.takePicture()),
             flatMap((imageAsset: ImageAsset) => ImageSource.fromAsset(imageAsset)),
             map((imageSource: ImageSource) => {
-                const len_photo = this.task_photo.length+1
-                const fileName = this.task_name + len_photo + ".jpg";
-                const photoFilePath = this.createPhotoPath(fileName);
-                // Save photo in full quality to file
-                const success: boolean = imageSource.saveToFile(photoFilePath, 'jpg');
-                if (!success) {
-                    throw new Error('Error during saving photo image to file ' + photoFilePath);
+                try{
+                    const len_photo = this.task_photo.length+1
+                    const fileName = this.task_name + len_photo + ".jpg";
+                    const photoFilePath = this.createPhotoPath(fileName);
+
+                    // Save photo in full quality to file
+                    const success: boolean = imageSource.saveToFile(photoFilePath, 'jpg');
+                    if (!success) {
+                        throw new Error('Error during saving photo image to file ' + photoFilePath);
+                    }
+                    else{
+                        // save photo success
+                        this.task_photo.push(photoFilePath)
+                        this.hasImage = true; // set image visible
+                    }
+                    return photoFilePath;
                 }
-                return photoFilePath;
+                catch(e){
+                    // when did not enter task title
+                    return alert('Please entered task title')
+                }
             })
-        ).subscribe(photoFilePath => {
-            console.log('Photofilepath ' + photoFilePath);
-            this.imagePath = photoFilePath;
-            this.task_photo.push(this.imagePath)
-        }, error => {
-            console.log(error);
-        })
+        )
+    }
+
+    public pickPhoto() {
+        let that = this;
+        let context = imagepicker.create({
+            mode: "multiple"
+        });
+        
+        context
+            .authorize()
+            .then(function() {
+                return context.present();
+            })
+            .then(function(selection) {
+                selection.forEach(function(selected) {
+                    // process the selected image
+                    let source = ImageSource.fromAsset(selected)
+                    source.then((imageSource: ImageSource) => {
+                        const len_photo = that.task_photo.length+1
+
+                        try {
+                            let fileName = that.task_name + len_photo + ".jpg";
+                            const photoFilePath = that.createPhotoPath(fileName);
+                            const success: boolean = imageSource.saveToFile(photoFilePath, 'jpg');
+
+                            if (!success) {
+                                throw new Error('Error during saving photo image to file ' + photoFilePath);
+                            }
+                            else{
+                                that.imagePath = photoFilePath;
+                                that.task_photo.push(that.imagePath)
+                                that.hasImage = true; // set image visible
+                            }
+                            return photoFilePath;
+                        }
+                        catch (e){
+                            // when did not enter task title
+                            return alert('Please entered task title')
+                        }
+                    })
+                })
+            }).catch(function (e) {
+                console.log(e);
+            });
     }
 
     private createPhotoPath(fileName: string): string {
@@ -96,7 +147,7 @@ export class AddTaskComponent {
         if (!photoFile) {
             throw new Error('Cannot create photo file');
         }
-        this.hasImage = true; // set image visible
         return photoFile.path;
     }
+
 }
