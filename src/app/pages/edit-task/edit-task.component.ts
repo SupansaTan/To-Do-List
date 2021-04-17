c7d1c84d84321e8e3df9aca4b79ffbe7a75e16e5
Suphawit Auchariyammet <s6201012620333@email.kmutnb.ac.th>
2021-04-17 18:41:44 +0700

    edit page html create

--- src/app/pages/edit-task/edit-task.component.ts
+++ src/app/pages/edit-task/edit-task.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component} from "@angular/core";
+import { ActivatedRoute} from '@angular/router';
+import { Task } from "~/app/task";
+import { TaskService } from "../../task.service";
+import { Location} from "@angular/common";
 
 @Component ({
     selector: "edit-task",
     templateUrl: "./edit-task.component.html",
-    
+    styleUrls: ['./edit-task.component.css'],
 })
 
 export class EditTaskComponent {
