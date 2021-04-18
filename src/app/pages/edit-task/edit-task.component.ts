e0005a709311128647e6c2f59b35616bd28b42e5
Suphawit Auchariyammet <s6201012620333@email.kmutnb.ac.th>
2021-04-18 20:30:00 +0700

    create function for edit task page

--- src/app/pages/edit-task/edit-task.component.ts
+++ src/app/pages/edit-task/edit-task.component.ts
@@ -1,8 +1,14 @@
-import { Component} from "@angular/core";
 import { ActivatedRoute} from '@angular/router';
-import { Task } from "~/app/task";
-import { TaskService } from "../../task.service";
-import { Location} from "@angular/common";
+import { Component } from "@angular/core";
+import { TaskService } from "~/app/task.service";
+import { Location } from '@angular/common'
+import * as camera from "@nativescript/camera";
+import { ImageAsset } from "@nativescript/core";
+import { ImageSource, knownFolders} from '@nativescript/core';
+import { flatMap, map } from 'rxjs/operators';
+import { from } from 'rxjs';
+import * as imagepicker from "@nativescript/imagepicker";
+import { Router } from "@angular/router";
 
 @Component ({
     selector: "edit-task",
@@ -13,13 +19,129 @@ import { Location} from "@angular/common";
 export class EditTaskComponent {
 
     task;
+    task_name: string;
+    task_detail: string;
+    task_date: Date;
+    task_photo: Array<string> = [];
+    task_notify: boolean;
+    hasImage: boolean;
+    imagePath: string | undefined;
+    
     constructor(public route: ActivatedRoute,
                public taskService: TaskService,
-               public location: Location) {}
+               public location: Location,
+               public router: Router) {}
+    
     ngOnInit() {
-     const routeParams = this.route.snapshot.paramMap;
-     const id = Number(routeParams.get('id'));
-     this.task = this.taskService.getTask(id);
+        const routeParams = this.route.snapshot.paramMap;
+        const id = Number(routeParams.get('id'));
+        this.task = this.taskService.getTask(id);
+        this.task_name=this.task.name
+        this.task_detail=this.task.detail
+        this.task_date=this.task.due_date
+        this.task_photo=this.task.photo
+        this.task_notify=this.task.notify
+    }
 
-}
+    public edit(){ 
+        this.taskService.editTask(this.task.id,this.task_name, this.task_detail, this.task_date, this.task_photo, this.task_notify)
+    }
+    
+    public getRandomString() {
+        // random hash generator for generate file name
+        var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var result = '';
+        for ( var i = 0; i < 16; i++ ) {
+            result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+        }
+        return result;
+    }
+
+    public takePhoto(): void {
+        if (!camera.isAvailable()) {
+            throw new Error('Camera not available');
+        }
+        from(camera.requestPermissions()).pipe(
+            flatMap(() => camera.takePicture()),
+            flatMap((imageAsset: ImageAsset) => ImageSource.fromAsset(imageAsset)),
+            map((imageSource: ImageSource) => {
+                // set photo name and path
+                const fileName = this.getRandomString() + ".jpg";
+                const photoFilePath = this.createPhotoPath(fileName);
+
+                // save photo in full quality to file
+                const success: boolean = imageSource.saveToFile(photoFilePath, 'jpg');
+                if (!success) {
+                    throw new Error('Error during saving photo image to file ' + photoFilePath);
+                }
+                return photoFilePath;
+            })
+        ).subscribe(photoFilePath => {
+            this.task_photo.push(photoFilePath)
+            this.hasImage = true; // set image visible
+            console.log('Photofilepath ' + photoFilePath);
+        }, error => {
+            console.log(error);
+        })
+    }
+
+    public pickPhoto() {
+        let that = this;
+        let context = imagepicker.create({
+            mode: "multiple"
+        });
+        
+        context
+            .authorize()
+            .then(function() {
+                return context.present();
+            })
+            .then(function(selection) {
+                selection.forEach(function(selected) {
+                    // process the selected image
+                    let source = ImageSource.fromAsset(selected)
+                    source.then((imageSource: ImageSource) => {
+                        // set photo name and path & save it
+                        let fileName = that.getRandomString() + ".jpg";
+                        const photoFilePath = that.createPhotoPath(fileName);
+                        const success: boolean = imageSource.saveToFile(photoFilePath, 'jpg');
+
+                        if (!success) {
+                            throw new Error('Error during saving photo image to file ' + photoFilePath);
+                        }
+                        else {
+                            // save successfully
+                            that.imagePath = photoFilePath;
+                            that.task_photo.push(that.imagePath)
+                            that.hasImage = true; // set image visible
+                        }
+                        return photoFilePath;
+                    })
+                })
+            }).catch(function (e) {
+                console.log(e);
+            });
+    }
+
+    private createPhotoPath(fileName: string): string {
+        const documentFolders = knownFolders.documents();
+        if (!documentFolders) {
+            throw new Error('Documents folder is not available');
+        }
+
+        // gets or creates photo folder
+        let photoFolderPath;
+        photoFolderPath = documentFolders.getFolder('images');
+
+        // gets or creates empty file
+        const photoFile = photoFolderPath.getFile(fileName);
+        if (!photoFile) {
+            throw new Error('Cannot create photo file');
+        }
+        return photoFile.path;
+    }
+
+    public photoViewer(src: string){
+        this.router.navigate(['/photo', src ]);
+    }
 }
